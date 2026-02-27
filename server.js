0563fe4753caa94f0ed36c85839655c8dae7488a
xiiiclub <szuedlak@gmail.com>
2026-02-27 15:50:41 +0100

    Update server.js
    update on fonts

--- server.js
+++ server.js
@@ -78,12 +78,12 @@ const formattedText = text.replace(/\\n/g, '\n');
   <div class="wrap">
     <div style="
   font-family: 'Picaflor';
-  font-size: 60px;
+  font-size: 40px;
   color: white;
   text-align: center;
   letter-spacing: 2px;
   line-height: 1.2;
-  max-width: 80%;
+  max-width: 70%;
   white-space: pre-wrap;
 ">
   ${formattedText}
@@ -117,6 +117,7 @@ const formattedText = text.replace(/\\n/g, '\n');
 app.get("/", (_, res) => res.send("OK - use /render?text=..."));
 
 app.listen(PORT, () => console.log(`Render server running on :${PORT}`));
+
 
 
 
